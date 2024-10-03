33c4ac4bf6dc4a8fa17ff845b9d1337ba1c5f611
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-10-03 04:15:10 -0400

    first commit

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ app.get('/download-zip', (req, res) => {
 });
 
 // 17 - Servidor y Socket.io en ejecución
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
 	console.log('Servidor y Socket.io en ejecución en http://localhost:3000');
 });
--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE,
+    host     : process.env.DB_HOST || "127.0.0.1",
+    user     : process.env.DB_USER || "root",
+    password : process.env.DB_PASS || "",
+    database : process.env.DB_DATABASE || "sw",
 });
 
 connection.connect((error) => {
