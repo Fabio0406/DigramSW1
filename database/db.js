1aabdc11c03eb20425c457de0bbb1f7dbbf9d5cf
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-10-03 04:30:24 -0400

    first commit

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
+const mysql = require('mysql2/promise');
+const connection = mysql.createPool({
     host     : process.env.DB_HOST || "127.0.0.1",
     user     : process.env.DB_USER || "root",
     password : process.env.DB_PASS || "",
     database : process.env.DB_DATABASE || "sw",
-
+    port:   process.env.DB_PORT || "3306"
 });
 
 connection.connect((error) => {
